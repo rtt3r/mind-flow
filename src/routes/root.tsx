7a174769e7231aec15ef809a3b8c6e3f69d9abcc
Anderson Ritter de Souza <anderson.ritter@terceiroslinx.com.br>
2024-12-12 23:50:39 -0300

    refact

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,17 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../components/header';
-import { Sidenav } from '../components/sidenav';
 
 export const Root = () => {
   return (
-    <main className="flex h-screen">
-      <Sidenav />
-      <div className="flex flex-col w-full">
-        <Header />
-        <section className="flex h-full w-full px-4 py-7">
-          <Outlet />
-        </section>
-      </div>
+    <main>
+      <Outlet />
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,55 +7,44 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   prefix: '',
   theme: {
-  	fontFamily: {
-  		sans: [
-  			'Cairo',
-                ...defaultTheme.fontFamily.sans
-            ],
-  		serif: [
-                ...defaultTheme.fontFamily.serif
-            ]
-  	},
-  	extend: {
-  		colors: {
-  			blue: 'hsl(var(--blue))',
-  			black: 'hsl(var(--black))',
-  			yellow: 'hsl(var(--yellow))',
-  			orange: 'hsl(var(--orange))',
-  			green: 'hsl(var(--green))',
-  			red: 'hsl(var(--red))',
-  			white: 'hsl(var(--white))',
-  			'gray-1': 'hsl(var(--gray-1))',
-  			'gray-2': 'hsl(var(--gray-2))',
-  			'gray-3': 'hsl(var(--gray-3))',
-  			'gray-4': 'hsl(var(--gray-4))',
-  			cyan: 'hsl(var(--cyan))',
-  			'dark-background': 'hsl(var(--dark-background))',
-  			'dark-1': 'hsl(var(--dark-1))',
-  			'dark-2': 'hsl(var(--dark-2))',
-  			'linear-1': 'linear-gradient(hsl(var(--linear-1-from)), hsl(var(--linear-1-to)))',
-  			'linear-2': 'linear-gradient(hsl(var(--linear-2-from)), hsl(var(--linear-2-to)))',
-  			'linear-3': 'linear-gradient(hsl(var(--linear-3-from)), hsl(var(--linear-3-to)))',
-  			'linear-4': 'linear-gradient(hsl(var(--linear-4-from)), hsl(var(--linear-4-to)))',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    fontFamily: {
+      sans: ['Cairo', ...defaultTheme.fontFamily.sans],
+      serif: [...defaultTheme.fontFamily.serif],
+    },
+    extend: {
+      colors: {
+        blue: 'hsl(var(--blue))',
+        black: 'hsl(var(--black))',
+        yellow: 'hsl(var(--yellow))',
+        orange: 'hsl(var(--orange))',
+        green: 'hsl(var(--green))',
+        red: 'hsl(var(--red))',
+        white: 'hsl(var(--white))',
+        'gray-1': 'hsl(var(--gray-1))',
+        'gray-2': 'hsl(var(--gray-2))',
+        'gray-3': 'hsl(var(--gray-3))',
+        'gray-4': 'hsl(var(--gray-4))',
+        cyan: 'hsl(var(--cyan))',
+        'dark-background': 'hsl(var(--dark-background))',
+        'dark-1': 'hsl(var(--dark-1))',
+        'dark-2': 'hsl(var(--dark-2))',
+        'linear-1':
+          'linear-gradient(hsl(var(--linear-1-from)), hsl(var(--linear-1-to)))',
+        'linear-2':
+          'linear-gradient(hsl(var(--linear-2-from)), hsl(var(--linear-2-to)))',
+        'linear-3':
+          'linear-gradient(hsl(var(--linear-3-from)), hsl(var(--linear-3-to)))',
+        'linear-4':
+          'linear-gradient(hsl(var(--linear-4-from)), hsl(var(--linear-4-to)))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
   plugins: [require('tailwindcss-animate')],
 };
